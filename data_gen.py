8a21491f0ddac8948965190c6cc37ddf447099b3
Yi <yliu16@ualberta.ca>
2020-08-24 17:00:50 -0600

    1

--- data_gen.py
+++ data_gen.py
@@ -6,10 +6,12 @@ def data_gen(Nk=10, K=4, p=2000, d=4, seed=None):
         np.random.seed(seed % (2 ** 31))
     X = np.random.randn(Nk * K, p)
     Y = np.arange(K).repeat(Nk)
-    scale = np.sqrt(1 + 0.5 * d ** 2)  # scale for first two dimensions
+    scale = np.sqrt(1 + 0.5 * (d ** 2))  # scale for first two dimensions
     for i in range(K):
-        X[i * Nk:(i + 1) * Nk, 0] += d * np.cos((i / K - 0.125) * np.pi * 2) / scale
-        X[i * Nk:(i + 1) * Nk, 1] += d * np.sin((i / K - 0.125) * np.pi * 2) / scale
+        X[i * Nk:(i + 1) * Nk, 0] += d * np.cos((i / K - 0.125) * np.pi * 2)
+        X[i * Nk:(i + 1) * Nk, 1] += d * np.sin((i / K - 0.125) * np.pi * 2)
+    X[:, 0] /= scale
+    X[:, 1] /= scale
     # print(X)
     return X, Y
 
